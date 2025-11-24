2bdf0faae0ccca98b286cde955dc488296893893
A-leks-andr <calllamurzakov@gmail.com>
2025-11-24 16:53:01 +0300

    add and change files 7 lesson

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-def index(request):
-    context = {
-        'app_name': 'hexlet_django_blog.article',
-        'page_title': 'Главная страница блога',
-        'welcome_message': 'Добро пожаловать в блог!',
-    }
-    return render(request, 'articles/index.html', context)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'app_name': 'hexlet_django_blog.article',
+            'page_title': 'Главная страница блога',
+            'welcome_message': 'Добро пожаловать в блог!',
+        }
+        return render(
+            request,
+            'articles/index.html',
+            context
+        )
