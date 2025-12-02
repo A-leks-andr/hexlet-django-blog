b9963cc9d6c3e296c8d7d5da7456733e9794d19d
A-leks-andr <calllamurzakov@gmail.com>
2025-12-02 06:14:50 +0300

    add and change files 15 lesson

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,6 +4,9 @@ from hexlet_django_blog.article import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="article"),
     path(
-        "<str:tags>/<int:article_id>/", views.ArticleTagsId.as_view(), name="article_id"
+        "<str:tags>/<int:article_id>/",
+        views.ArticleTagsId.as_view(),
+        name="article_tags",
     ),
+    path("<int:id>", views.ArticleView.as_view(), name="article_id"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from hexlet_django_blog.article.models import Article
 
@@ -16,3 +16,15 @@ class ArticleTagsId(View):
         tags = self.kwargs["tags"]
         article_id = self.kwargs["article_id"]
         return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "articles/show.html",
+            context={
+                "article": article,
+            },
+        )
